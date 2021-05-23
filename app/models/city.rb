1573009d928150158283aaf321456eedc5861a53
Mike Wilder <mikewilder01@gmail.com>
2021-05-23 16:14:15 -0400

    commit tree 225c89b584e544cb786613a0db1b910264804b96

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,24 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  
+
+  def city_openings(arrive, leave) 
+    
+    a = Date.parse(arrive)
+    l = Date.parse(leave)
+    openings = []
+    #binding.pry
+    self.listings.each do |e| 
+      e.reservations do |r|
+        #binding.pry
+        if a >= r.checkout and  l <= r.checkin
+          return e
+        end
+      end
+    end
+    
+  end
+
 end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,14 @@
 class Listing < ActiveRecord::Base
+  validates :address, presence: true
+
+
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
+
+  
+
   
 end
